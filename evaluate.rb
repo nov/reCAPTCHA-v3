acc320ac6d493064ccfbcfa36f8b64e97d5340bb
Nov Matake <nov@matake.jp>
2018-05-14 13:43:02 +0900

    up

--- evaluate.rb
+++ evaluate.rb
@@ -2,11 +2,13 @@ require 'rack/oauth2'
 
 Rack::OAuth2.debug!
 
-token = '03AJpayVHZB2g7vQDHAyZAV_2siabu_MMezfkhtpNHbromtP-AZCG8SrFT_g8_XiN_MB8YaVaB0lxQ6PRoS0_Q3ut7i0EhTncSPbTUd2Wn6FHTH2t38JNtfM_gV8kqds_FGitebLrvr2CyiSn3NNWufmBkVmPujc6O2mG-6hcmnb9OzVSzS_3Thk2Tb5dFuVFnYTFbLyluveplUef94Iklk8ltGq4-LOxhfoElmFA8cSptCQepLK9JM3U9kmJi-cmEPdtc_hew8wybetTGz5ZiiFD0idRK_1jVUF3OTI2zoWXM46y-AnndVH4lMnW6XTPT9EGZL0AEmSyc'
+token = '03AJpayVEMGk74tezvAd7Mazg5nXB--6CO4swxdWHQVQAL7xP0XLvxl_yffISHLhl2B2oNSJataYQx48muDixgKizILlnjBxBlklWzJuLSLEfWbfKh3ObIp-FwE172CsIkHkb1cq-SonSrBEZtyPF9fHor3Z_GV5QqALzn7nsjJQKIk13FqzErHPv2YwV7WFn43qMlZ5QM3NV94QXf9vn4wFtQY7l8PdHqcU-2PZM7YWBFnGPePmiKEBeMTa18kpMDD5yK3umLBZvvzFa2vu5ZGOt4fkyGdckUfXT2SGtEguXCLHlKu81yE7uuge9U5sAnhiqFny1xVtApYgdRgDVKnuzt3mrDrFNWkg'
 
-Rack::OAuth2.http_client.post(
+response = Rack::OAuth2.http_client.post(
   'https://www.google.com/recaptcha/api/siteverify', {
     secret: '6LetCFkUAAAAALFUB0ZLgTimtOy6A2YlTU3hcwC5',
     response: token
   }
 )
+
+puts response.body
